429ceb5c541a90d8f6550211fce5ebdc1edaa54c
Bhayazeed <bhayazeed@gmail.com>
2024-05-11 23:45:24 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="container">
+    <h1>INI WEB KU</h1>
+    <input type="text" v-model="newTask" placeholder="Masukan Tesk...">
+    <button @click="addTask">Tambahkan Teks</button>
+    <ul>
+      <li v-for="(task, index) in tasks" :key="index">
+        <span>{{ task }}</span>
+        <button @click="editTask(index)">Edit</button>
+        <button @click="deleteTask(index)">Delete</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTask: '',
+      tasks: []
+    };
+  },
+  methods: {
+    addTask() {
+      if (this.newTask.trim() !== '') {
+        this.tasks.push(this.newTask.trim());
+        this.newTask = '';
+      }
+    },
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+    },
+    editTask(index) {
+      const newTask = prompt('Edit task:', this.tasks[index]);
+      if (newTask !== null) {
+        this.tasks[index] = newTask.trim();
+      }
+    }
+  }
+};
+</script>
+
+<style>
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 20px;
+}
+input[type="text"] {
+  width: 70%;
+  padding: 10px;
+  margin-right: 10px;
+}
+button {
+  padding: 10px 20px;
+  background-color: #4CAF50;
+  color: white;
+  border: none;
+  cursor: pointer;
+}
+button:hover {
+  background-color: #45a049;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  margin-bottom: 10px;
+}
+</style>
